09b13924e4702620e7c8b589b019724b9adc9290
Alexis Paboul <alexispaboul@gmail.com>
2025-11-05 10:59:50 +0000

    Added margin and purchase_cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,29 @@
+with
+sales as (
+  select 
+  date_date,
+  orders_id,
+  products_id,
+  quantity,
+  revenue
+   from {{ ref('stg_raw__sales') }}
+),
+product as (
+  select 
+  products_id,
+  CAST(purchase_price as FLOAT64) AS purchase_price
+  from {{ ref('stg_raw__product') }}
+)
+
+select 
+s.date_date,
+s.orders_id,
+s.products_id,
+s.quantity,
+s.revenue,
+p.purchase_price,
+ROUND((s.quantity * p.purchase_price),2) AS purchase_cost,
+ROUND((s.revenue - (s.quantity * p.purchase_price)),2) AS margin
+from sales s
+left join product p
+  using (products_id)
