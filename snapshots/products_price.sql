44fd05375d58df1ac418b46f4406be3deaf98d7d
Alexis Paboul <alexispaboul@gmail.com>
2025-11-06 13:43:19 +0000

    snapshot

--- snapshots/products_price.sql
+++ snapshots/products_price.sql
@@ -1,14 +1,18 @@
 {% snapshot products_snapshot %}
 
 {{
-    config(
-      target_schema='dbt_snapshots', --corresponding to the new database in BigQuery
-      unique_key='products_id', --unique key of the table to track
-      strategy='check', --other strategy
-      check_cols='all' --track evolution on all columns
-    )
+  config(
+    target_schema = 'dbt_snapshots',
+    unique_key    = 'products_id',
+    strategy      = 'check',
+    check_cols    = ['purchase_price']
+  )
 }}
 
-select * from {{ ref("stg_raw__product") }}
+select
+  products_id,
+  purchase_price,
+  category
+from {{ ref('stg_raw__product') }}
 
 {% endsnapshot %}
